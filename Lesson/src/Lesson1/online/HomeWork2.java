a88d0dd8898e3a434588fd7cb34323eed3ab8cbc
Rustan <dj-cat@mail.ru>
2021-01-30 17:32:21 +0300

    homework2

---
+++ Lesson/src/Lesson1/online/HomeWork2.java
@@ -0,0 +1,10 @@
+package Lesson1.online;
+
+public class HomeWork2 {
+    public static boolean task10and20 ( int x1, int x2){
+        int sum = x1 + x2;
+        return sum >= 10 && sum <= 20;
+
+    }
+
+}
