eb7bc24466a3eebf0ab504ba62a7fde82a61ba9c
Rustan <dj-cat@mail.ru>
2021-01-30 17:31:59 +0300

    homework4

---
+++ Lesson/src/Lesson1/online/HomeWork4.java
@@ -0,0 +1,9 @@
+package Lesson1.online;
+
+public class HomeWork4 {
+    public static void greetings(String name){
+        System.out.println("Привет" + name);
+
+    }
+}
+// в консоли появляеться только "Process finished with exit code 0"?
