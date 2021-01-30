fdabaad0b53b10154de132de7fb55bbb63792748
Ivanovrus23 <78260873+Ivanovrus23@users.noreply.github.com>
2021-01-30 17:53:21 +0300

    Add files via upload

---
+++ Lesson/src/Lesson1/HomeWork.java
@@ -0,0 +1,17 @@
+package Lesson1.online;
+
+public class HomeWork {
+    public static void main(String[] args) {
+    }
+
+
+    public static void isPositiveOrNegative(int x) {
+        if (x >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+
+
+    }
+}// в консоли появляеться только "Process finished with exit code 0"?
